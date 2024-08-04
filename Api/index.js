e458e56412c10a21337d570d494f720aba0a55d1
salman Khan <salmankhanm859@gmail.com>
2024-08-04 20:19:21 +0500

    commit

--- Api/index.js
+++ Api/index.js
@@ -1,23 +1,29 @@
 import express from 'express';
-
 import mongoose from 'mongoose';
-
 import cors from 'cors';
-import adminAuth from './routes/AdminAuth.js'; // Add the .js extension
-import UserDataAuth from './routes/UserDataAuth.js';
+import adminAuth from '../routes/AdminAuth.js';
+import UserDataAuth from '../routes/UserDataAuth.js';
+import { config } from 'dotenv';  // Correctly import `config` from `dotenv`
+import serverless from 'serverless-http';
+
 const app = express();
 
-app.use(cors());
+// Load environment variables from .env file
+config();  
 
+app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://Salman:admin@cluster0.jshy5un.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+// Connect to MongoDB using URI from environment variables
+const mongoUri = process.env.MONGO_URI || 'mongodb+srv://Salman:admin@cluster0.jshy5un.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(mongoUri)
     .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.log(err))
+    .catch((err) => console.log('MongoDB connection error:', err));
 
+// Set up routes
+app.use('/api/user', adminAuth);
+app.use('/api/userData', UserDataAuth);
 
-app.use('/api/user',adminAuth);
-app.use('/api/userData',UserDataAuth)
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+// Export the serverless function
+export const handler = serverless(app);
